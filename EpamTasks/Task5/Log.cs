3c0d685b2110f3d16a340f3cc6e8f97f9cea406f
Trindoz <50882557+Trindoz@users.noreply.github.com>
2019-12-23 17:35:15 +0400

    Aded Task5

---
+++ EpamTasks/Task5/Log.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace Task5
+{
+    class DataFile
+    {
+        public FileInfo[] GetAtributes(string path)
+        {    
+                DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ып\Desktop\Task5\Archives");
+                FileInfo[] txtfiles = dir.GetFiles("*.txt");
+                return txtfiles;      
+        }
+        public FileStream  GetContentFile(string path) 
+        {
+          return File.OpenRead(path);
+        }
+
+    }
+    public class Log
+    {
+        public static void Writing(string date)
+        {
+        Dictionary<string, FileInfo[]> files = new Dictionary<string, FileInfo[]>();
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ып\Desktop\Task5\Archives");
+            FileInfo[] txtfiles = dir.GetFiles("*.txt");  
+            files.Add(date, txtfiles);               
+            string serialized = JsonConvert.SerializeObject(files);
+            File.AppendAllText(@"C:\Users\ып\Desktop\Task5\.txt", serialized);
+        }        
+        public static void Changes(string current)
+        {
+            string sourceDir = current;
+            string backupDir = @"C:\Users\ып\Desktop\Task5\Archives";
+
+            try
+            {
+                string[] txtList = Directory.GetFiles(sourceDir, "*.txt");
+                foreach (string f in txtList)
+                {
+                    string fName = f.Substring(sourceDir.Length + 1);
+                    File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName), true);
+                }
+            }
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+        }
+    }
+    }
+
+
+
+
---
+++ EpamTasks/Task5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+
+
+namespace Task5
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            try
+            {
+                watcher.Path = @"C:\Users\ып\Desktop\Task5\Current";
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Size | NotifyFilters.CreationTime;
+            watcher.Filter = "*.txt";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Press q to exit");
+            while (Console.Read() != 'q') ;
+        }
+        
+        
+        static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Log.Changes(@"C:\Users\ып\Desktop\Task5\Current");
+        }
+        static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            Log.Changes(@"C:\Users\ып\Desktop\Task5\Current");
+
+
+            Console.WriteLine("File: {0} renamed to {1} !", e.OldFullPath, e.FullPath);
+        }
+        static void OnDeleted(object source, FileSystemEventArgs e)
+        {
+
+            Log.Changes(@"C:\Users\ып\Desktop\Task5\Current");
+        }
+        static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Log.Changes(@"C:\Users\ып\Desktop\Task5\Current");
+        }
+
+    }
+}
