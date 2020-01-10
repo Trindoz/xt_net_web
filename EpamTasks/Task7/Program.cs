36fc37bdf7c502bbf45154247cd9d0f4e06a6f28
Trindoz <50882557+Trindoz@users.noreply.github.com>
2020-01-10 14:52:19 +0400

    Added Task6,7

--- EpamTasks/Task7/Program.cs
+++ EpamTasks/Task7/Program.cs
@@ -11,14 +11,69 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            DateExistance();
-            HtmlReplacer();
-            EmailFinder();
-            NumberValidator();
+            Run();
+        }
+        public static void Run()
+        {
+            Console.WriteLine("Please, choose a task");
+            Console.WriteLine("0: Exit");
+            Console.WriteLine("1: Date Existance");
+            Console.WriteLine("2: HTML Replacer");
+            Console.WriteLine("3: Email Finder");
+            Console.WriteLine("4: Number Validator");
+            Console.WriteLine("5: Time Counter");
+            int mode = Exception(Console.ReadLine());
+            switch (mode)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    DateExistance();
+                    Run();
+                    break;
+                case 2:
+                    HtmlReplacer();
+                    Run();
+                    break;
+                case 3:
+                    EmailFinder();
+                    Run();
+                    break;
+                case 4:
+                    NumberValidator();
+                    Run();
+                    break;
+                case 5:
+                    TimeCounter();
+                    Run();
+                    break;
+                default:
+                    Console.WriteLine("Cannot find apropriate task, please check the input");
+                    Run();
+                    break;
+            }
+        }
+        static int Exception(string s)
+        {
+            int x;
+            bool result = Int32.TryParse(s, out x);
+            if (result) ;
+            else
+            {
+                do
+                {
+                    Console.WriteLine("Incorrect input");
+                    result = Int32.TryParse(Console.ReadLine(), out x);
+                }
+                while (result == false);
+            }
+            return x;
         }
         public static void DateExistance()
         {
-            string input = "Введите текст, содержащий дату в формате dd-mm-yyyy: 2016 год наступит 01-01-2016";
+            Console.WriteLine("Please put a text ");
+            string input = Console.ReadLine();
             Regex regex = new Regex(@"\d{1,2}-\d{1,2}-\d{4}");
             MatchCollection matches = regex.Matches(input);
             if (matches.Count > 0)
@@ -38,7 +93,8 @@ namespace Task7
         }
         public static void EmailFinder()
         {
-            string input = "Иван:ivan@mail.ru,Петр:p_ivanov@mail.rol.ru,Семен:54-Semen.zak-Harov@list.com";
+            Console.WriteLine("Please put a text ");
+            string input = Console.ReadLine();
             Regex regex = new Regex(@"\w+([.-]\w+)*@\w+([.]\w+)*");
             MatchCollection matches = regex.Matches(input);
             if (matches.Count > 0)
@@ -51,6 +107,7 @@ namespace Task7
         }
         public static void NumberValidator()
         {
+            Console.WriteLine("Please put a text ");
             string input = Console.ReadLine();
             string simple = @"(-\d+[.,]+\d+)|(\d+[.,]+\d+)|\d+";
             string scientific = @"(\d.+([=,.e-]+\d+))|\d.+[e]|\d+[e]";
@@ -68,5 +125,23 @@ namespace Task7
                 Console.WriteLine("Not number"); 
             }
         }
+        public static void TimeCounter()
+        {
+            Console.WriteLine("Please put a text ");
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\b(([0-9])|(0[0-9])|(1[0-9])|(2[0-3])):[0-5][0-9]\b");
+            MatchCollection matches = regex.Matches(input);
+            int i = 0;
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    i++;                  
+                }
+                Console.WriteLine("Time has founded " +i);
+            }
+            else { Console.WriteLine("No time in text"); }
+        }
+
     }
 }
